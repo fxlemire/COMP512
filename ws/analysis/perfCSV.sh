c1a0de49489b1e37c0729fea9bf14b2d90ac4366
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-11-12 18:54:24 -0500

    csv generator for method performance analysis

---
+++ ws/analysis/perfCSV.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+path=$1
+outputFile=$2
+
+echo "method,time" > $outputFile
+
+cat $path | grep "\[PERF\]" $path | while read -r line ; do
+    stringarray=($line)
+    echo "${stringarray[1]},${stringarray[3]}" >> $outputFile
+done
